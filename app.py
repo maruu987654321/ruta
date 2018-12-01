55d890de571780fc50fab273bd23550bdab9900e
mariy <45437484+ruta777@users.noreply.github.com>
2018-12-01 05:25:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,7 @@ def homepage():
     return(pytesseract.image_to_string(Image.open('1.jpg')).encode('utf-8'))
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
